ea43eac510b7a1d4a6ed01452115b371e3518398
thanhnd3103 <133090491+thanhnd3103@users.noreply.github.com>
2024-07-06 23:25:09 +0700

    Update status return of Transaction

--- API/Controllers/TransactionController.cs
+++ API/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 using BLL.Services.Interfaces;
 using Common.RequestObjects.Transaction;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API.Controllers;
 
@@ -16,6 +17,7 @@ public class TransactionController : ControllerBase
     }
 
     [HttpPut("/orders/{orderId}/transaction")]
+    [SwaggerOperation(Summary = "This will update the Transaction's status of the OrderID to PAID, also the bank code ")]
     public ActionResult<object> UpdateTransaction([FromBody] UpdateTransactionRequest updateTransactionRequest, int orderId)
     {
         var response = _transactionService.UpdateTransaction(updateTransactionRequest, orderId);

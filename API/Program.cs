2ab0e853de3ca752abc624a771f018e366dcb0b4
HighLink1001 <caolinh1012003@gmail.com>
2024-07-22 18:33:40 +0700

    Fix enum

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ var config = builder.Configuration;
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    // options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
